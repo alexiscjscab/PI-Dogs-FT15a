3a5bcb00d25cace2fbf44e208e6f35166c74d89c
alexiscjscab <alexiscjscab@gmail.com>
2021-08-01 20:05:43 -0300

    rutas creadas dogs.js funcionan faltan las demas

---
+++ api/src/routes/dogs.js
@@ -0,0 +1,77 @@
+const { Router } = require('express');
+const { Dog } = require('../db');
+const axios = require('axios');
+const { API_KEY } = process.env;
+const router = Router();
+const {Op} = require('sequelize');
+
+
+// pagina de la api
+const URL = `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`;
+
+router.get('/', async (req, res) => {
+    
+    // recibimos name por query parameter   
+    const {name} = req.query;
+
+
+    // si no ingresamos name
+    if(!name){
+        //hacemos un try, catch
+        try{
+            let resultsApi = await axios.get(URL);
+            
+            // al resultApi el json lo convierto en un array
+            let dogsApi = [...resultsApi.data];
+
+            // si existe entonces hago un map y asi tengo lo que necesito
+            dogsApi = await dogsApi && dogsApi.map(data => ({
+                name: data.name,
+                id: data.id,
+                life_span: data.life_span,
+                weight: data.weight.metric,
+                height: data.height.metric,
+                temperament: data.temperament,
+                image: data.image.url
+            }));
+
+            let resultsDb = await Dog.findAll()
+
+            // con el ... concateno dogsApi y con el resultado de la base de datos
+            let totalDogs = [...dogsApi, ...resultsDb];
+
+            // console.log(totalDogs.length) hay 172
+            console.log(totalDogs)
+            return res.json(totalDogs)
+
+        }
+        catch(error){
+            return res.send(error);
+        }
+    }
+
+    // si existe el name
+    if(name){
+        try{
+            let resultsApi = await axios.get(URL);
+
+            let dogApiFilter = resultsApi.data.filter( dog => dog.name.toLowerCase().includes(`${name.toLowerCase()}`));
+
+            let resultsDb = await Dog.findAll({
+                where:{name:{
+                    [Op.iLike] : `%${name}%`
+                }}
+            });
+
+            let dogsName = [...dogApiFilter, ... resultsDb];
+
+            return res.json(dogsName)
+        }catch(error){
+            res.send(error);
+        }
+    }
+
+});
+
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogs = require('./dogs');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dogs', dogs);
 
 module.exports = router;
