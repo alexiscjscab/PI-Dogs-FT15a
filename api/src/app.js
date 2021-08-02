a821aca85a146c7354aa950a2f96bd0b5f00da88
alexiscjscab <alexiscjscab@gmail.com>
2021-08-01 21:05:53 -0300

    app agregado de cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require ('cors');
 
 require('./db.js');
 
@@ -10,6 +11,7 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors());
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
